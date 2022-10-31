fd0ce18cf305e75f3eb65e59a71a261180c379e1
sussycylinder <115450379+sussycylinder@users.noreply.github.com>
2022-10-31 14:50:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
-basic.forever(function () {
-	
+let xvalue = 0
+let yvalue = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    xvalue = 0
+    yvalue = 0
+    for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(xvalue, yvalue)
+            basic.pause(500)
+            xvalue += 1
+        }
+        yvalue += 1
+        for (let index = 0; index < 5; index++) {
+            xvalue += -1
+            led.plot(xvalue, yvalue)
+            basic.pause(500)
+        }
+        yvalue += 1
+    }
 })
